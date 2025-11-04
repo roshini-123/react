236afef5f37bf91436c41c5eda949e3f03736411
Roshini <godaroshini76@gmail.com>
2025-11-04 10:24:01 +0530

    comonent

---
+++ react-git/src/ComponentFile.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function ComponentNew () {
+    return (
+        <div>
+            <h1>Hello World</h1>
+            </div>
+    )
+}
+
+export default ComponentNew;
