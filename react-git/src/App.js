e0676b642c77e1e5a38010c77388471160136e99
roshini-123 <godaroshini76@gmail.com>
2026-05-31 13:13:01 +0530

    chnages

--- react-git/src/App.js
+++ react-git/src/App.js
@@ -1,53 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
-import ComponentNew from './ComponentFile';
-import Work from './Nestedcomp';
-import CssStyling from './ElementFile';
-import GettingImage from './ImageCreating';
-import FirstComponent from './PropsContainer/FirstComp';
-import ThirdComponenet from './PropsContainer/ThirdComp';
-import FourthCompoenent from './PropsContainer/FourthComp';
-import SecondComponent from './PropsContainer/secondComp';
-import ChildComponent from './PropsDestr/PropsChildComp';
-import UseStateHook from './StateMng.js/UseStateHook';
-import UseEffectHook from './StateMng.js/UseEffectHook';
-import TerneryOperatorUse from './TerneryOperator.js/TerneryOpe';
-import OnClickEvent from './EventsFold/onClickEve';
-import EventList from './EventsFold/eventLis';
 
-
-const user = {
-  "name":"Roshini",
-  "age":26,
-  "born":"Feb 7th"
-}
-
-const propsDestrObj = {
-  "year":"2012",
-  "month":"Dec",
-  "bike":"Royal enfiled",
-  "model":"Hunter"
-}
-
+import HeaderObj from './donar-module/HeaderObj';
+import footerObj from './donar-module/FooterObj';
+import FooterObj from './donar-module/FooterObj';
 
 
 function App() {
   return (
-    <div>
-      <EventList/>
-      <ComponentNew/>
-      <Work/>
-      <CssStyling/>
-      <GettingImage/>
-      <FirstComponent name = {user.name}/>
-      <SecondComponent name = {user.age}/>
-      <ThirdComponenet name = {user.born}/>
-      <FourthCompoenent name  = "Vinayaka"/>
-      <ChildComponent car = {propsDestrObj}/>
-      <UseStateHook/>
-      <UseEffectHook/>
-      <TerneryOperatorUse/>
-      <OnClickEvent/>
+    <div className = "bg-black-800"> 
+      <HeaderObj/>
+      <FooterObj/>
     </div>
   );
 }
---
+++ react-git/src/donar-module/HeaderObj.js
@@ -0,0 +1,14 @@
+import react from "react";
+
+
+const HeaderObj = () => {
+    return (
+        <div>
+            <div className = "fixed top-0 right-0 w-[209px] h-[87px] bg-yellow-500 rounded-lg"></div>
+        </div>
+    )
+
+}
+
+export default HeaderObj
+
